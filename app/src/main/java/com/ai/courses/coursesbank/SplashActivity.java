0e02de3f84eaaa614f492db297682f6739f0e5d4
collin-Kipkorir <collinkipkorir@gmail.com>
2024-02-13 19:31:50 +0300

    lotta

--- app/src/main/java/com/ai/courses/coursesbank/SplashActivity.java
+++ app/src/main/java/com/ai/courses/coursesbank/SplashActivity.java
@@ -1,36 +1,48 @@
 package com.ai.courses.coursesbank;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
-import android.os.Bundle;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.os.Bundle;
 import android.os.CountDownTimer;
-
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private int mSplash_Timer = 6000; // Splash screen timer in milliseconds
-    private CountDownTimer mTimeCounter = new CountDownTimer(mSplash_Timer, 100) {
-        @Override
-        public void onTick(long millisUntilFinished) {
-            // Not used in this example
-        }
-
-        @Override
-        public void onFinish() {
-            mNextPage(new Intent(SplashActivity.this, MainActivity.class));
-            finish();
-        }
-    };
+    private static final int SPLASH_TIMER = 6000; // Splash screen timer in milliseconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        mTimeCounter.start();
-    }
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.white)));
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        // Change the color of the status bar
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.white));
+        }
+        new CountDownTimer(SPLASH_TIMER, 100) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                // Not used in this example
+            }
 
-    private void mNextPage(Intent intent) {
-        startActivity(intent);
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(SplashActivity.this, WelcomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }.start();
     }
 }
